105cfedb7dd4b5d458b4c19a08d25d6ba5442647
Fabian Affolter <mail@fabian-affolter.ch>
2021-12-04 10:04:18 +0100

    python3Packages.mdurl: init at 0.1.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4758,6 +4758,8 @@ in {
 
   mdit-py-plugins = callPackage ../development/python-modules/mdit-py-plugins { };
 
+  mdurl = callPackage ../development/python-modules/mdurl { };
+
   MDP = callPackage ../development/python-modules/mdp { };
 
   measurement = callPackage ../development/python-modules/measurement { };
