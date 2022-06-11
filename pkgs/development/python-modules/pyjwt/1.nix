e260feaa032d07ca1b7199c180b1902c3431fce8
Robert Schütz <nix@dotlambda.de>
2022-06-11 17:30:25 +0000

    python310Packages.pyjwt_1: fix CVE-2022-29217

--- pkgs/development/python-modules/pyjwt/1.nix
+++ pkgs/development/python-modules/pyjwt/1.nix
@@ -1,4 +1,5 @@
 { lib
+, fetchpatch
 , buildPythonPackage
 , fetchPypi
 , cryptography
@@ -13,6 +14,18 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "15hflax5qkw1v6nssk1r0wkj83jgghskcmn875m3wgvpzdvajncd";
   };
+
+  patches = [
+    (fetchpatch {
+      name = "CVE-2022-29217.patch";
+      url = "https://github.com/jpadilla/pyjwt/commit/9c528670c455b8d948aff95ed50e22940d1ad3fc.patch";
+      excludes = [
+        ".gitignore"
+        "jwt/__init__.py" # changes version
+      ];
+      sha256 = "sha256-9fgCn2Pb3q72c4w5utP16KhDsAe50EnHd7Z57Zrj7wA=";
+    })
+  ];
 
   postPatch = ''
     substituteInPlace setup.py --replace "pytest>=4.0.1,<5.0.0" "pytest"
@@ -34,8 +47,5 @@ buildPythonPackage rec {
     description = "JSON Web Token implementation in Python";
     homepage = "https://github.com/jpadilla/pyjwt";
     license = licenses.mit;
-    knownVulnerabilities = [
-      "CVE-2022-29217"
-    ];
   };
 }
