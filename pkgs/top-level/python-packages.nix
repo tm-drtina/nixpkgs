b25c240983e135bd8beb053b43b944c760cd2511
Robert Scott <code@humanleg.org.uk>
2022-10-02 18:23:46 +0100

    python3Packages.configupdater: init at 3.1.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -1728,6 +1728,8 @@ in {
 
   configshell = callPackage ../development/python-modules/configshell { };
 
+  configupdater = callPackage ../development/python-modules/configupdater { };
+
   confluent-kafka = callPackage ../development/python-modules/confluent-kafka { };
 
   confuse = callPackage ../development/python-modules/confuse { };
